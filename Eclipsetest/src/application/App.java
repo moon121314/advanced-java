c4ae833528d984b0964a24bcab2ffee13a4fe890
moon121314 <kmrmuun@gmail.com>
2025-11-04 03:11:18 +0600

    create a project

---
+++ Eclipsetest/src/application/App.java
@@ -0,0 +1,11 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -1,4 +1,3 @@
-package app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
